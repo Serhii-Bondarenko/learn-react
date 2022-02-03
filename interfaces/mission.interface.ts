70bb82247b62fc792483c8520e1482dd7c4f842a
Serhii-Bondarenko <sergeybondd@gmail.com>
2022-02-03 20:14:32 +0200

    typescript homework

---
+++ interfaces/mission.interface.ts
@@ -0,0 +1,36 @@
+export interface IMission{
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string;
+    };
+    links: ILinks;
+    rocket: {
+        rocket_name: string;
+        first_stage: {
+            cores: [ICores];
+        }
+    }
+    second_stage: {
+        payloads: [IPayloads];
+    }
+}
+
+interface ILinks{
+    article_link: null;
+    video_link: string;
+}
+
+interface ICores{
+    flight: number;
+    core: {
+        reuse_count: number;
+        status: string;
+    }
+}
+
+interface IPayloads{
+    payload_type: string;
+    payload_mass_kg: number;
+    payload_mass_lbs: number;
+}
